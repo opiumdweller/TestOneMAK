592804ec1d58e8b70887d948286cb8821b93e48f
opiumdweller <mkeislin@saintjoe.edu>
2016-02-26 13:38:51 -0500

    Initial commit. Code tested and works. Prints "Michael Keisling" six times.

---
+++ TestOneMAK/src/edu/saintjoe/cs/opiumdweller/testonemak/TestOneMak.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.opiumdweller.testonemak;
+
+public class TestOneMak {
+	public static void main(String[] args) {
+		System.out.println("Michael Keisling");
+		System.out.println("Michael Keisling");
+		System.out.println("Michael Keisling");
+		System.out.println("Michael Keisling");
+		System.out.println("Michael Keisling");
+		System.out.println("Michael Keisling");
+	}
+	}
